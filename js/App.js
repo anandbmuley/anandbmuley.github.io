f246d6f68b2d1e56e9b53036f236f51b98879fff
AnandMuley <muley.anand66@gmail.com>
2017-08-27 00:58:13 +0530

    [AM] Working on ObjectOrientedProgramming section

--- js/App.js
+++ js/App.js
@@ -1,12 +1,14 @@
 var app = angular.module('AppTech',['ngMaterial','ngRoute']);
 
 app.config(['$routeProvider',function($routeProvider){
-  
+
   $routeProvider.when('/',{
     templateUrl:'views/HomePage.html',
     controller : 'HomePageController'
   }).when('/home',{
     redirectTo : '/'
+  }).when('/java',{
+    templateUrl:'views/java/ObjectOrientedProgramming.html'
   });
 
 }]);
