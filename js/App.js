c01c70fbf45f482c2596d8fba0b5825076b37edd
Anand Muley <muley.anand66@gmail.com>
2020-05-30 00:20:33 +0530

    [ABM]-Added Java Datatype Blog

--- js/App.js
+++ js/App.js
@@ -1,61 +1,63 @@
-var app = angular.module('AppTech',['ngMaterial','ngRoute']);
+var app = angular.module('AppTech', ['ngMaterial', 'ngRoute']);
 
-app.config(['$routeProvider',function($routeProvider){
+app.config(['$routeProvider', function ($routeProvider) {
 
-  $routeProvider.when('/',{
-    templateUrl:'views/LandingPage.html',
-    controller : 'LandingPageController'
-  }).when('/home',{
+  $routeProvider.when('/', {
+    templateUrl: 'views/LandingPage.html',
+    controller: 'LandingPageController'
+  }).when('/home', {
     templateUrl: 'views/HomePage.html',
-    controller : 'HomePageController'
-  }).when('/java',{
-    templateUrl:'views/java/JavaHome.html',
-    controller : 'JavaHomeController'
-  }).when('/code',{
-    templateUrl : 'views/code/CodeHome.html',
-    controller : 'CodeHomeController'
-  }).when('/research',{
-    templateUrl : 'views/research/ResearchHome.html',
+    controller: 'HomePageController'
+  }).when('/java', {
+    templateUrl: 'views/java/JavaHome.html',
+    controller: 'JavaHomeController'
+  }).when('/code', {
+    templateUrl: 'views/code/CodeHome.html',
+    controller: 'CodeHomeController'
+  }).when('/research', {
+    templateUrl: 'views/research/ResearchHome.html',
     controller: 'ResearchHomeController'
-  }).when('/java/object-oriented-design',{
-    templateUrl:'views/java/ObjectOrientedProgramming.html',
+  }).when('/java/data-types', {
+    templateUrl: 'views/java/DataTypes.html'
+  }).when('/java/object-oriented-design', {
+    templateUrl: 'views/java/ObjectOrientedProgramming.html',
     controller: 'ScrollToTopController'
-  }).when('/java/let-code-do-the-talking',{
-    templateUrl:'views/java/let-code-do-the-talking/Home.html',
-    controller : 'LetCodeDoTheTalking'
-  }).when('/java/let-code-do-the-talking/domain-modelling',{
-    templateUrl : 'views/java/let-code-do-the-talking/DomainModelling.html',
+  }).when('/java/let-code-do-the-talking', {
+    templateUrl: 'views/java/let-code-do-the-talking/Home.html',
     controller: 'LetCodeDoTheTalking'
-  }).when('/java/objects-vs-classes',{
-    templateUrl : 'views/java/ObjectsVsClasses.html',
+  }).when('/java/let-code-do-the-talking/domain-modelling', {
+    templateUrl: 'views/java/let-code-do-the-talking/DomainModelling.html',
+    controller: 'LetCodeDoTheTalking'
+  }).when('/java/objects-vs-classes', {
+    templateUrl: 'views/java/ObjectsVsClasses.html',
     controller: 'ScrollToTopController'
-  }).when('/java/design-patterns-is-fish',{
+  }).when('/java/design-patterns-is-fish', {
     templateUrl: 'views/java/research/DesignPatternsIsAFish.html',
     controller: 'ScrollToTopController'
-  }).when('/general',{
+  }).when('/general', {
     templateUrl: 'views/general/GeneralHome.html',
-    controller : 'GeneralHomeController'
-  }).when('/general/evolution-of-languages',{
-    templateUrl : 'views/general/EvolutionOfProgrammingLanguages.html',
+    controller: 'GeneralHomeController'
+  }).when('/general/evolution-of-languages', {
+    templateUrl: 'views/general/EvolutionOfProgrammingLanguages.html',
     controller: 'ScrollToTopController'
-  }).when('/general/entrusted-freedom-on-developers',{
+  }).when('/general/entrusted-freedom-on-developers', {
     templateUrl: 'views/general/EntrustedFreedomOnDevelopers.html',
     controller: 'ScrollToTopController'
-  }).when('/general/charity-at-workplace',{
-    templateUrl : 'views/general/CharityAtWorkplace.html',
+  }).when('/general/charity-at-workplace', {
+    templateUrl: 'views/general/CharityAtWorkplace.html',
     controller: 'ScrollToTopController'
-  }).when('/general/content-duplication-on-internet',{
-    templateUrl : 'views/general/ContentDuplicationOnInternet.html',
+  }).when('/general/content-duplication-on-internet', {
+    templateUrl: 'views/general/ContentDuplicationOnInternet.html',
     controller: 'ScrollToTopController'
-  }).when('/general/centralised-object-creation',{
-    templateUrl : 'views/general/CentralisedObjectCreation.html',
+  }).when('/general/centralised-object-creation', {
+    templateUrl: 'views/general/CentralisedObjectCreation.html',
     controller: 'ScrollToTopController'
-  }).when('/research/history-of-programming-paradigm',{
-    templateUrl : 'views/research/HistoryOfProgramminParadigm.html',
+  }).when('/research/history-of-programming-paradigm', {
+    templateUrl: 'views/research/HistoryOfProgramminParadigm.html',
     controller: 'ScrollToTopController'
   })
-  .when('/about-me',{
-    templateUrl:'views/AboutMe.html'
-  });
+    .when('/about-me', {
+      templateUrl: 'views/AboutMe.html'
+    });
 
 }]);
--- js/controllers/JavaHomeController.js
+++ js/controllers/JavaHomeController.js
@@ -1,39 +1,46 @@
-app.controller('JavaHomeController',['$scope','$location',
-  function($scope,$location){
+app.controller('JavaHomeController', ['$scope', '$location',
+  function ($scope, $location) {
 
-  $scope.topics = [
-    {
-      'heading':'Object Oriented Design',
-      'subHeading':'Thinking in terms of objects',
-      'imageUrl':'object_oriented.jpg',
-      'route':'/java/object-oriented-design',
-      'imageCredits' : 'Created by D3images - Freepik.com'
-    },
-    {
-      'heading':'Let code do the talking',
-      'subHeading':'Code speaks for itself',
-      'imageUrl':'laptop.jpg',
-      'route':'/java/let-code-do-the-talking',
-      'imageCredits' : 'Created by fullvector - Freepik.com'
-    },
-    {
-      'heading':'Objects vs Classes',
-      'subHeading':'Which one to use when',
-      'imageUrl':'ObjectsVsClasses.jpg',
-      'route':'/java/objects-vs-classes',
-      'imageCredits' : 'Created by alekksall - www.freepik.com'
-    },
-    {
-      'heading':'Design Pattern is a Fish',
-      'subHeading':'Learn to Design a Pattern',
-      'imageUrl':'DesignPatternIsAFish.jpeg',
-      'route':'/java/design-patterns-is-fish',
-      'imageCredits' : 'Created by brgfx - www.freepik.com'
+    $scope.topics = [
+      {
+        'heading': 'Object Oriented Design',
+        'subHeading': 'Thinking in terms of objects',
+        'imageUrl': 'object_oriented.jpg',
+        'route': '/java/object-oriented-design',
+        'imageCredits': 'Created by D3images - Freepik.com'
+      },
+      {
+        'heading': 'Let code do the talking',
+        'subHeading': 'Code speaks for itself',
+        'imageUrl': 'laptop.jpg',
+        'route': '/java/let-code-do-the-talking',
+        'imageCredits': 'Created by fullvector - Freepik.com'
+      },
+      {
+        'heading': 'Objects vs Classes',
+        'subHeading': 'Which one to use when',
+        'imageUrl': 'ObjectsVsClasses.jpg',
+        'route': '/java/objects-vs-classes',
+        'imageCredits': 'Created by alekksall - www.freepik.com'
+      },
+      {
+        'heading': 'Design Pattern is a Fish',
+        'subHeading': 'Learn to Design a Pattern',
+        'imageUrl': 'DesignPatternIsAFish.jpeg',
+        'route': '/java/design-patterns-is-fish',
+        'imageCredits': 'Created by brgfx - www.freepik.com'
+      },
+      {
+        'heading': 'Data Types',
+        'subHeading': 'Basic and Advanced',
+        'imageUrl': 'java/BuildingMaterials.jpg',
+        'route': '/java/data-types',
+        'imageCredits': 'Created by bpch.vector - www.freepik.com'
+      }
+    ];
+
+    $scope.loadPage = function (route) {
+      $location.path(route);
     }
-  ];
 
-  $scope.loadPage = function (route){
-    $location.path(route);
-  }
-
-}]);
+  }]);
