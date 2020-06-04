b375b7c823e11203dfee9cb21be05b4ff9f455d4
Anand Muley <muley.anand66@gmail.com>
2020-06-04 11:40:14 +0530

    [ABM]-Made the website responsive

--- js/controllers/NavigationController.js
+++ js/controllers/NavigationController.js
@@ -1,48 +1,48 @@
-app.controller('NavigationController',['$scope','$location','$rootScope',
-          function($scope,$location,$rootScope){
+app.controller('NavigationController', ['$scope', '$location', '$rootScope',
+  function ($scope, $location, $rootScope) {
 
-  $scope.currentNavItem = 'home';
+    $scope.currentNavItem = 'home';
 
-  function identifyUrl(){
-    if($location.url() === '/'){
-      $rootScope.isLandingPage = true;
-    } else {
+    function identifyUrl() {
+      if ($location.url() === '/') {
+        $rootScope.isLandingPage = true;
+      } else {
+        $rootScope.isLandingPage = false;
+      }
+    }
+
+    identifyUrl();
+
+    $scope.navigations = [
+      {
+        displayName: "Home",
+        navUrl: "home"
+      },
+      {
+        displayName: "Java",
+        navUrl: "java"
+      },
+      {
+        displayName: "General",
+        navUrl: "general"
+      },
+      {
+        displayName: "Research",
+        navUrl: "research"
+      },
+      {
+        displayName: "CODE",
+        navUrl: "code"
+      },
+      {
+        displayName: "About Me",
+        navUrl: "about-me"
+      }
+    ];
+
+    $scope.goto = function (pageName) {
       $rootScope.isLandingPage = false;
+      $location.path(pageName);
     }
-  }
 
-  identifyUrl();
-
-  $scope.navigations = [
-    {
-      displayName : "Home",
-      navUrl      : "home"
-    },
-    {
-      displayName : "Java",
-      navUrl      : "java"
-    },
-    {
-      displayName : "General",
-      navUrl      : "general"
-    },
-    {
-      displayName : "Research",
-      navUrl      : "research"
-    },
-    {
-      displayName : "CODE",
-      navUrl      : "code"
-    },
-    {
-      displayName : "About Me",
-      navUrl      : "about-me"
-    }
-  ];
-
-  $scope.goto = function(pageName){
-    $rootScope.isLandingPage = false;
-    $location.path(pageName);
-  }
-
-}]);
+  }]);
